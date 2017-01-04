76451d66f40981987a537851f9bbde54db6e6e35
apatronl <apl7@gatech.edu>
2017-01-04 14:15:03 -0600

    Added gif caption functionality

--- GifMaker/GifEditorViewController.swift
+++ GifMaker/GifEditorViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class GifEditorViewController: UIViewController {
+class GifEditorViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var gifImageView: UIImageView!
     @IBOutlet weak var captionTextField: UITextField!
@@ -18,17 +18,75 @@ class GifEditorViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         gifImageView.image = gif?.gifImage
+        subscribeToKeyboardNotifications()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        captionTextField.delegate = self
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    // MARK: - UITextFieldDelegate
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        captionTextField.placeholder = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        captionTextField.resignFirstResponder()
+        if let caption = captionTextField.text {
+            let trimmed = caption.trimmingCharacters(in: .whitespacesAndNewlines)
+            if trimmed.isEmpty {
+                captionTextField.text = nil
+                captionTextField.placeholder = "Add a caption"
+            }
+        }
+        return true
     }
     
 }
+
+// Methods to adjust the keyboard
+extension GifEditorViewController {
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(GifEditorViewController.keyboardWillShow),
+                                                         name: NSNotification.Name.UIKeyboardWillShow,
+                                                         object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(GifEditorViewController.keyboardWillHide),
+                                                         name: NSNotification.Name.UIKeyboardWillHide,
+                                                         object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if view.frame.origin.y >= 0 {
+            view.frame.origin.y -= getKeyboardHeight(notification: notification)
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if (self.view.frame.origin.y < 0) {
+            view.frame.origin.y += getKeyboardHeight(notification: notification)
+        }
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+}
+
