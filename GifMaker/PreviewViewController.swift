0bec2e20c6363e186d0d24e7ae66ee13c19e1dff
apatronl <apl7@gatech.edu>
2017-01-06 09:36:48 -0600

    Added gif shring

--- GifMaker/PreviewViewController.swift
+++ GifMaker/PreviewViewController.swift
@@ -21,18 +21,19 @@ class PreviewViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func shareGif(sender: AnyObject) {
+        let animatedGif = NSData(contentsOf: (self.gif?.url!)!)
+        let itemsToShare = [animatedGif]
+        
+        let shareController = UIActivityViewController(activityItems: itemsToShare, applicationActivities: nil)
+        shareController.completionWithItemsHandler = {(activity, completed, items, error) in
+            if completed {
+                _ = self.navigationController?.popToRootViewController(animated: true)
+            }
+        }
+        self.navigationController?.present(shareController, animated: true, completion: nil)
     }
-    */
 
 }
